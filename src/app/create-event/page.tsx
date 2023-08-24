2bc8246f2a81b6db4c6c1cf01b9163fb97cb4bc2
trandat1114 <cctalk1114@gmail.com>
2023-08-25 00:22:30 +0700

    good in devnet

--- src/app/create-event/page.tsx
+++ src/app/create-event/page.tsx
@@ -6,7 +6,7 @@ export default function Page() {
       <div className='grid grid-cols-1 md:grid-cols-12 items-center gap-5 h-full'>
         <div className='col-span-1 md:col-span-6'>
           <p className='font-extrabold text-4xl text-primary uppercase'>Create Event</p>
-
+          <p>Already have event now! and wanna add ticket</p>
         </div>
         <div className='col-span-1 md:col-span-6'>
           <p className="text-bold text-xl text-primary font-mono">Input your event information here!</p>
